43dbb3cdad1fe665510a3a485865083564d7a37f
shahzebgit <shahzebparkar@gmail.com>
2020-04-09 10:00:00 +0530

    App js file

--- z-clothing/src/App.js
+++ z-clothing/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect'
- 
+import { createStructuredSelector } from 'reselect';
+
 import './App.css';
-import Header from './components/header/header.component'
+
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop pages/shop'
-import SignInPage from './pages/Sign In and Reg/sign-in.component'
-import CheckoutPage from './pages/checkout/checkout.components'
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
-import { setCurrentUser } from './Redux/user/user-action';
-import { selectCurrentUser } from './Redux/user/user.selector'
+import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -56,10 +58,9 @@ class App extends React.Component {
               this.props.currentUser ? (
                 <Redirect to='/' />
               ) : (
-                  <SignInPage />
-                )
+                <SignInAndSignUpPage />
+              )
             }
-    
           />
         </Switch>
       </div>
@@ -67,7 +68,7 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = createStructuredSelector ({
+const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
