c2b408776e37a049e6d629584421ee93fd537c52
shahzebgit <shahzebparkar@gmail.com>
2020-04-22 10:00:00 +0530

    Update homepage.component.jsx

--- z-clothing/src/pages/homepage/homepage.component.jsx
+++ z-clothing/src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import Directory from '../../components/directory/directory.compoenent';
+import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
-
-const HomePage = () =>(
-    <div className='homepage'>
+const HomePage = () => (
+  <HomePageContainer>
     <Directory />
-    </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
