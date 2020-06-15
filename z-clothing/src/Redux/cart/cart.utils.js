518c63ca36a0d66e2ad4c4cf460693fb6d990d8e
shahzebgit <shahzebparkar@gmail.com>
2020-06-15 17:16:58 +0530

    Update cart.utils.js

--- z-clothing/src/Redux/cart/cart.utils.js
+++ z-clothing/src/Redux/cart/cart.utils.js
@@ -1,30 +1,31 @@
-export const addItemToCart = (cartItems, cartItemtoAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemtoAdd.id);
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
 
-if(existingCartItem){
-    return cartItems.map(cartItem => 
-        cartItem.id === cartItemtoAdd.id ?
-        {...cartItem,quantity:cartItem.quantity + 1}
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
-        )
-}
+    );
+  }
 
-return [...cartItems,{...cartItemtoAdd,quantity : 1}]
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
 
-export const removeItemFromCart =(cartItems ,cartItemToRemove)=>{
-    const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToRemove.id
-    )
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
 
-    if(existingCartItem.quantity === 1){
-        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
 
-    }
-    return cartItems.map(
-        cartItem=>
-        cartItem.id ===  cartItemToRemove.id ?
-        {...cartItem ,quantity: cartItem.quantity - 1}
-        : cartItem
-    )
-}
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
