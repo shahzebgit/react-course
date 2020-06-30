e8d87f8a54cdf0fbe380c8ae55e1290f0609dfeb
shahzebgit <shahzebparkar@gmail.com>
2020-06-30 10:00:00 +0530

    Added UserSaga to root

--- z-clothing/src/redux/root-saga.js
+++ z-clothing/src/redux/root-saga.js
@@ -1,8 +1,7 @@
-import {all,call} from 'redux-saga/effects';
-import { shopSaga } from './shop/shop.sagas';
+import { all, call } from "redux-saga/effects";
+import { shopSaga } from "./shop/shop.sagas";
+import { userSaga } from "./user/user.sagas";
 
-
-export default function* rootSaga(){
-    yield all([call(shopSaga)]);
+export default function* rootSaga() {
+  yield all([call(shopSaga), call(userSaga)]);
 }
-
