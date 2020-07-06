acdef0b7eb99bcf43fe7b07a8b6eecd05d30f3dd
shahzebgit <shahzebparkar@gmail.com>
2020-07-06 15:52:29 +0530

    changes

--- z-clothing/src/redux/root-saga.js
+++
@@ -1,7 +0,0 @@
-import { all, call } from "redux-saga/effects";
-import { shopSaga } from "./shop/shop.sagas";
-import { userSaga } from "./user/user.sagas";
-
-export default function* rootSaga() {
-  yield all([call(shopSaga), call(userSaga)]);
-}
