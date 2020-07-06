07aaa275c62ad38d5e7a218145808adc26290a21
shahzebgit <shahzebparkar@gmail.com>
2020-07-06 15:55:34 +0530

    Update App.js

--- z-clothing/src/App.js
+++ z-clothing/src/App.js
@@ -13,11 +13,15 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
+  componentDidMount(){
+    const {checkUserSession} =this.props
+    checkUserSession();
+  }
 
-  
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
@@ -51,6 +55,11 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
+const mapDispatchToProps = dispatch =>({
+  checkUserSession:()=>dispatch(checkUserSession())
+})
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(App);
